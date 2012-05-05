9b662e36d8765e0b848b6024b069b1929ead4342
Marc Villacorta <marc.villacorta@gmail.com>
2012-05-05 18:57:48 +0200

    Added vimrc to setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ FILE="root/.bash_profile";     [ ! -f /${FILE} ] && cp ${BASE}/${FILE} /${FILE}
 FILE="root/.bashrc";           [ ! -f /${FILE} ] && cp ${BASE}/${FILE} /${FILE}
 FILE="etc/skel/.bash_profile"; [ ! -h /${FILE} ] && [ -f /${FILE} ] && rm -f /${FILE} && ln -s ${BASE}/${FILE} /${FILE}
 FILE="etc/skel/.bashrc";       [ ! -h /${FILE} ] && [ -f /${FILE} ] && rm -f /${FILE} && ln -s ${BASE}/${FILE} /${FILE}
+FILE="etc/vimrc";              [ ! -h /${FILE} ] && [ -f /${FILE} ] && rm -f /${FILE} && ln -s ${BASE}/${FILE} /${FILE}
