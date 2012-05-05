d590b14463b5aabc1597939077f63f685473a9e3
Marc Villacorta <marc.villacorta@gmail.com>
2012-05-05 20:02:43 +0200

    Added xinitrc

--- setup.sh
+++ setup.sh
@@ -5,5 +5,6 @@ FILE="root/.bash_profile";     [ ! -f /${FILE} ] && cp ${BASE}/${FILE} /${FILE}
 FILE="root/.bashrc";           [ ! -f /${FILE} ] && cp ${BASE}/${FILE} /${FILE}
 FILE="etc/skel/.bash_profile"; [ ! -h /${FILE} ] && [ -f /${FILE} ] && rm -f /${FILE} && ln -s ${BASE}/${FILE} /${FILE}
 FILE="etc/skel/.bashrc";       [ ! -h /${FILE} ] && [ -f /${FILE} ] && rm -f /${FILE} && ln -s ${BASE}/${FILE} /${FILE}
+FILE="etc/skel/.xinitrc";      [ ! -h /${FILE} ] && [ -f /${FILE} ] && rm -f /${FILE} && ln -s ${BASE}/${FILE} /${FILE}
 FILE="etc/vimrc";              [ ! -h /${FILE} ] && [ -f /${FILE} ] && rm -f /${FILE} && ln -s ${BASE}/${FILE} /${FILE}
 FILE="etc/pacman.conf";        [ ! -h /${FILE} ] && [ -f /${FILE} ] && rm -f /${FILE} && ln -s ${BASE}/${FILE} /${FILE}
