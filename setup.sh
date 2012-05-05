fd7f47dd67017248e9767899522c73c7a5285677
Marc Villacorta <marc.villacorta@gmail.com>
2012-05-05 18:49:40 +0200

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+BASE="/etc/.dotfiles"
+FILE="root/.bash_profile";     [ ! -f /${FILE} ] && cp ${BASE}/${FILE} /${FILE}
+FILE="root/.bashrc";           [ ! -f /${FILE} ] && cp ${BASE}/${FILE} /${FILE}
+FILE="etc/skel/.bash_profile"; [ ! -h /${FILE} ] && [ -f /${FILE} ] && rm -f /${FILE} && ln -s ${BASE}/${FILE} /${FILE}
+FILE="etc/skel/.bashrc";       [ ! -h /${FILE} ] && [ -f /${FILE} ] && rm -f /${FILE} && ln -s ${BASE}/${FILE} /${FILE}
