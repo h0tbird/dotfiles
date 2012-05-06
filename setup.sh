22d511e5d02b2bd7a0971e59e6643716abb222bd
Marc Villacorta <marc.villacorta@gmail.com>
2012-05-06 19:38:23 +0200

    Use 'cp -alf' to setup the files

--- setup.sh
+++ setup.sh
@@ -1,26 +1,3 @@
 #!/bin/sh
 
-BASE="/etc/.dotfiles"
-
-# /root
-FILE[0]="root/.bash_profile"
-FILE[1]="root/.bashrc"
-for i in ${FILE[@]}; do [ ! -f /${i} ] && cp ${BASE}/${i} /${i}; done
-
-# /etc
-FILE[0]="etc/vimrc"
-FILE[1]="etc/pacman.conf"
-FILE[2]="etc/sudoers"
-FILE[3]="etc/modprobe.d/blacklist.conf"
-for i in ${FILE[@]}; do [ ! /${i} -ef ${BASE}/${i} ] && rm -f /${i} && ln ${BASE}/${i} /${i}; done
-
-# /etc/skel
-DIR[0]="etc/skel/.config"
-DIR[1]="etc/skel/.config/awesome"
-for i in ${DIR[@]}; do [ ! -d /${i} ] && mkdir /${i} && chmod 0700 /${i}; done
-
-FILE[0]="etc/skel/.bash_profile"
-FILE[1]="etc/skel/.bashrc"
-FILE[2]="etc/skel/.xinitrc"
-FILE[3]="etc/skel/.config/awesome/rc.lua"
-for i in ${FILE[@]}; do [ ! /${i} -ef ${BASE}/${i} ] && rm -f /${i} && ln ${BASE}/${i} /${i}; done
+cp -alf rootfs/* /
