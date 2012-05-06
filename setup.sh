8ab40bddd10f78e34d23bb572d5241a4f08800a2
Marc Villacorta <marc.villacorta@gmail.com>
2012-05-06 13:06:35 +0200

    Replace softlinks with hardlinks

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@
 BASE="/etc/.dotfiles"
 FILE="root/.bash_profile";     [ ! -f /${FILE} ] && cp ${BASE}/${FILE} /${FILE}
 FILE="root/.bashrc";           [ ! -f /${FILE} ] && cp ${BASE}/${FILE} /${FILE}
-FILE="etc/skel/.bash_profile"; [ ! -h /${FILE} ] && [ -f /${FILE} ] && rm -f /${FILE} && ln -s ${BASE}/${FILE} /${FILE}
-FILE="etc/skel/.bashrc";       [ ! -h /${FILE} ] && [ -f /${FILE} ] && rm -f /${FILE} && ln -s ${BASE}/${FILE} /${FILE}
-FILE="etc/skel/.xinitrc";      [ ! -h /${FILE} ] && [ -f /${FILE} ] && rm -f /${FILE} && ln -s ${BASE}/${FILE} /${FILE}
-FILE="etc/vimrc";              [ ! -h /${FILE} ] && [ -f /${FILE} ] && rm -f /${FILE} && ln -s ${BASE}/${FILE} /${FILE}
-FILE="etc/pacman.conf";        [ ! -h /${FILE} ] && [ -f /${FILE} ] && rm -f /${FILE} && ln -s ${BASE}/${FILE} /${FILE}
-FILE="etc/sudoers";            [ ! -h /${FILE} ] && [ -f /${FILE} ] && rm -f /${FILE} && ln -s ${BASE}/${FILE} /${FILE}
+FILE="etc/skel/.bash_profile"; [ ! /${FILE} -ef ${BASE}/${FILE} -a -f /${FILE} ] && rm -f /${FILE} && ln ${BASE}/${FILE} /${FILE}
+FILE="etc/skel/.bashrc";       [ ! /${FILE} -ef ${BASE}/${FILE} -a -f /${FILE} ] && rm -f /${FILE} && ln ${BASE}/${FILE} /${FILE}
+FILE="etc/skel/.xinitrc";      [ ! /${FILE} -ef ${BASE}/${FILE} -a -f /${FILE} ] && rm -f /${FILE} && ln ${BASE}/${FILE} /${FILE}
+FILE="etc/vimrc";              [ ! /${FILE} -ef ${BASE}/${FILE} -a -f /${FILE} ] && rm -f /${FILE} && ln ${BASE}/${FILE} /${FILE}
+FILE="etc/pacman.conf";        [ ! /${FILE} -ef ${BASE}/${FILE} -a -f /${FILE} ] && rm -f /${FILE} && ln ${BASE}/${FILE} /${FILE}
+FILE="etc/sudoers";            [ ! /${FILE} -ef ${BASE}/${FILE} -a -f /${FILE} ] && rm -f /${FILE} && ln ${BASE}/${FILE} /${FILE}
