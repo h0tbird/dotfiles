495970190e1649bb652a764aa2d152101568d2eb
Marc Villacorta <marc.villacorta@gmail.com>
2012-05-06 14:17:39 +0200

    Added Awesome rc.lua

--- setup.sh
+++ setup.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
 BASE="/etc/.dotfiles"
-FILE="root/.bash_profile";     [ ! -f /${FILE} ] && cp ${BASE}/${FILE} /${FILE}
-FILE="root/.bashrc";           [ ! -f /${FILE} ] && cp ${BASE}/${FILE} /${FILE}
-FILE="etc/skel/.bash_profile"; [ ! /${FILE} -ef ${BASE}/${FILE} -a -f /${FILE} ] && rm -f /${FILE} && ln ${BASE}/${FILE} /${FILE}
-FILE="etc/skel/.bashrc";       [ ! /${FILE} -ef ${BASE}/${FILE} -a -f /${FILE} ] && rm -f /${FILE} && ln ${BASE}/${FILE} /${FILE}
-FILE="etc/skel/.xinitrc";      [ ! /${FILE} -ef ${BASE}/${FILE} -a -f /${FILE} ] && rm -f /${FILE} && ln ${BASE}/${FILE} /${FILE}
-FILE="etc/vimrc";              [ ! /${FILE} -ef ${BASE}/${FILE} -a -f /${FILE} ] && rm -f /${FILE} && ln ${BASE}/${FILE} /${FILE}
-FILE="etc/pacman.conf";        [ ! /${FILE} -ef ${BASE}/${FILE} -a -f /${FILE} ] && rm -f /${FILE} && ln ${BASE}/${FILE} /${FILE}
-FILE="etc/sudoers";            [ ! /${FILE} -ef ${BASE}/${FILE} -a -f /${FILE} ] && rm -f /${FILE} && ln ${BASE}/${FILE} /${FILE}
+
+# /root
+FILE[0]="root/.bash_profile"
+FILE[1]="root/.bashrc"
+for i in ${FILE[@]}; do [ ! -f /${i} ] && cp ${BASE}/${i} /${i}; done
+
+# /etc
+FILE[0]="etc/vimrc"
+FILE[1]="etc/pacman.conf"
+FILE[2]="etc/sudoers"
+for i in ${FILE[@]}; do [ ! /${i} -ef ${BASE}/${i} ] && rm -f /${i} && ln ${BASE}/${i} /${i}; done
+
+# /etc/skel
+DIR[0]="etc/skel/.config"
+DIR[1]="etc/skel/.config/awesome"
+for i in ${DIR[@]}; do [ ! -d /${i} ] && mkdir /${i} && chmod 0700 /${i}; done
+
+FILE[0]="etc/skel/.bash_profile"
+FILE[1]="etc/skel/.bashrc"
+FILE[2]="etc/skel/.xinitrc"
+FILE[3]="etc/skel/.config/awesome/rc.lua"
+for i in ${FILE[@]}; do [ ! /${i} -ef ${BASE}/${i} ] && rm -f /${i} && ln ${BASE}/${i} /${i}; done
