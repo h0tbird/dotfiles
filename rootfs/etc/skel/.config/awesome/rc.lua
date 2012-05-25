f23df68519694451202823630877e02a6de77732
Marc Villacorta <marc.villacorta@gmail.com>
2012-05-25 16:23:16 +0200

    Added opacity efect

--- rootfs/etc/skel/.config/awesome/rc.lua
+++ rootfs/etc/skel/.config/awesome/rc.lua
@@ -52,7 +52,7 @@ editor_cmd = terminal .. " -e " .. editor
 -- If you do not like this or do not have such a key,
 -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 -- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod1"
+modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 layouts =
@@ -398,6 +398,14 @@ client.add_signal("manage", function (c, startup)
     end
 end)
 
-client.add_signal("focus", function(c) c.border_color = beautiful.border_focus end)
-client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+client.add_signal("focus", function(c)
+    c.border_color = beautiful.border_focus
+    c.opacity = 1
+end)
+
+client.add_signal("unfocus", function(c)
+    c.border_color = beautiful.border_normal
+    c.opacity = 0.8
+end)
+
 -- }}}
