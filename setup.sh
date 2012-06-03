fc6ca7c344c649de6b6a5c964605de19ab6a3b3b
Marc Villacorta <marc.villacorta@gmail.com>
2012-06-03 14:17:34 +0200

    Cross-device improvement

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-cp -alf rootfs/* /
+BASEDIR="/etc/.dotfiles/rootfs"
+
+for i in `find ${BASEDIR} -type f | cut -b 23-`; do
+    cp -alf ${BASEDIR}/${i} /${i} 2> /dev/null || cp -f ${BASEDIR}/${i} /${i}
+done
