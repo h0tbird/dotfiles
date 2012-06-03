3d34f997fb7a53699440fb261259859252e673a4
Marc Villacorta <marc.villacorta@gmail.com>
2012-06-03 19:49:29 +0200

    Tip to ignore changes to tracked files

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@ BASEDIR="/etc/.dotfiles/rootfs"
 for i in `find ${BASEDIR} -type f | cut -b 23-`; do
     cp -alf ${BASEDIR}/${i} /${i} 2> /dev/null || cp -f ${BASEDIR}/${i} /${i}
 done
+
+# Ignore changes to tracked files:
+# git update-index --assume-unchanged path/to/tracked/file
+# git ls-files -v | grep -e "^[hsmrck]"
