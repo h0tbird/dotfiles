1bdb7992a54cda94017c533e134c4b7453dde242
Marc Villacorta <marc.villacorta@gmail.com>
2012-06-26 21:38:00 +0200

    Create full dirname

--- setup.sh
+++ setup.sh
@@ -3,10 +3,14 @@
 BASEDIR="/etc/.dotfiles/rootfs"
 
 for i in `find ${BASEDIR} -type f | cut -b 23-`; do
+    mkdir -p `dirname /${i}`
     cp -alf ${BASEDIR}/${i} /${i} 2> /dev/null || cp -f ${BASEDIR}/${i} /${i}
 done
 
+#----------------------------------
 # Ignore changes to tracked files:
+#----------------------------------
+
 # git update-index --assume-unchanged path/to/tracked/file
 # git ls-files -v | grep -e "^[hsmrck]"
 # git update-index --no-assume-unchanged path/to/tracked/file
