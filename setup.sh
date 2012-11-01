d826128417ab99820f8b1a7862aecc59ccd4145a
Marc Villacorta <marc.villacorta@gmail.com>
2012-11-01 18:03:20 +0100

    Remove initscripts dependency.

--- setup.sh
+++ setup.sh
@@ -7,11 +7,35 @@
 BASEDIR="/etc/.dotfiles/rootfs"
 FILES=`find $BASEDIR -type f | cut -b 23-`
 
+#-------------------
+# Helper functions:
+#-------------------
+
+C_CLEAR=$(tput sgr0)
+C_MAIN=${C_CLEAR}$(tput bold)
+C_OTHER=${C_MAIN}$(tput setaf 4)
+C_BUSY=${C_CLEAR}$(tput setaf 6)
+DEL_TEXT="\e[$(( $(( $(tput cols) - 13 )) + 4 ))G"
+
+deltext() {
+    printf "${DEL_TEXT}"
+}
+
+stat_busy() {
+    printf "${C_OTHER}:: ${C_MAIN}${1}${C_CLEAR} "
+    printf "\e[s"
+    deltext
+    printf "   ${C_OTHER}[${C_BUSY}BUSY${C_OTHER}]${C_CLEAR} "
+}
+
+stat_done() {
+    deltext
+    printf "   ${C_OTHER}[${C_MAIN}DONE${C_OTHER}]${C_CLEAR} \n"
+}
+
 #------------
 # Functions:
 #------------
-
-. /etc/rc.d/functions
 
 function exists() {
     stat_busy $1; [ -f "$1" ] && return 0 || return 1
