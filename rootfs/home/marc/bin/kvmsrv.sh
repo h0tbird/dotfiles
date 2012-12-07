29cfbfa84c470bbe4bb9188af7c7970318e16195
Marc Villacorta <marc.villacorta@gmail.com>
2012-12-07 21:03:58 +0100

    Auto detect network and priorize lan before wifi.

--- rootfs/home/marc/bin/kvmsrv.sh
+++ rootfs/home/marc/bin/kvmsrv.sh
@@ -1,28 +1,37 @@
 #!/bin/sh
 
-case $1 in
+systemctl --type service | grep -q '^network.*wifi.*loaded' && NET='wifi'
+systemctl --type service | grep -q '^network.*lan.*loaded' && NET='lan'
+
+case $NET in
 
     wifi ) qemu-kvm \
            -enable-kvm \
-           -smp 1,sockets=1,cores=1,threads=1 \
+           -smp 2,sockets=2,cores=1,threads=1 \
            -name puppet.demo.lan \
+           -nodefconfig \
+           -rtc base=utc \
            -drive file='/home/marc/CentOS.img',if=virtio \
            -net nic,model=virtio \
            -net tap,vlan=0,ifname=tap0,script=no,downscript=no \
+           -serial pty \
            -m 1024 \
-           -vnc 127.0.0.1:0 \
+           -nographic \
            -daemonize
            ;;
 
     lan ) qemu-kvm \
           -enable-kvm \
-          -smp 1,sockets=1,cores=1,threads=1 \
+          -smp 2,sockets=2,cores=1,threads=1 \
           -name puppet.demo.lan \
+          -nodefconfig \
+          -rtc base=utc \
           -drive file='/home/marc/CentOS.img',if=virtio \
           -net nic,model=virtio \
           -net bridge,helper='/usr/lib/qemu/qemu-bridge-helper' \
+          -serial pty \
           -m 1024 \
-          -vnc 127.0.0.1:0 \
+          -nographic \
           -daemonize
           ;;
 esac
