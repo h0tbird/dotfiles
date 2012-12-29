ca53aab38dd2ec03d10eb61d2e997df4b5ef78ce
Marc Villacorta <marc.villacorta@gmail.com>
2012-12-29 18:50:44 +0100

    Testing new ways to configure KVM networking.

--- rootfs/home/marc/bin/kvmsrv.sh
+++ rootfs/home/marc/bin/kvmsrv.sh
@@ -5,33 +5,48 @@ systemctl --type service | grep -q '^network.*lan.*loaded' && NET='lan'
 
 case $NET in
 
-    wifi ) qemu-system-x86_64 \
-           -enable-kvm \
-           -smp 2,sockets=2,cores=1,threads=1 \
-           -name puppet.demo.lan \
-           -nodefconfig \
-           -rtc base=utc \
-           -drive file='/home/marc/CentOS.img',if=virtio \
-           -net nic,model=virtio \
-           -net tap,vlan=0,ifname=tap0,script=no,downscript=no \
-           -serial pty \
-           -m 1024 \
-           -nographic \
-           -daemonize
-           ;;
+    #-------------------------------------------------------------
+    # ip tuntap add dev tap0 mode tap user marc
+    # ip link set dev tap0 up
+    # ip addr add 192.168.2.1/24 dev tap0 broadcast 192.168.2.255
+    #-------------------------------------------------------------
 
-    lan ) qemu-system-x86_64 \
-          -enable-kvm \
-          -smp 2,sockets=2,cores=1,threads=1 \
-          -name puppet.demo.lan \
-          -nodefconfig \
-          -rtc base=utc \
-          -drive file='/home/marc/CentOS.img',if=virtio \
-          -net nic,model=virtio \
-          -net bridge,helper='/usr/lib/qemu/qemu-bridge-helper' \
-          -serial pty \
-          -m 1024 \
-          -nographic \
-          -daemonize
-          ;;
+    wifi )  qemu-system-x86_64 \
+            -enable-kvm \
+            -smp 2,sockets=2,cores=1,threads=1 \
+            -name puppet01.demo.lan \
+            -nodefconfig \
+            -rtc base=utc \
+            -drive file='/home/marc/CentOS.img',if=virtio \
+            -net nic,model=virtio \
+            -net tap,vlan=0,ifname=tap0,script=no,downscript=no \
+            -serial pty \
+            -m 1024 \
+            -nographic \
+            -daemonize
+            ;;
+
+    #------------------------------------------------------------
+    # ip tuntap add dev tap0 mode tap user marc
+    # ip link set dev tap0 up
+    # brctl addbr br0
+    # brctl addif br0 eth0
+    # brctl addif br0 tap0
+    # ip addr add 192.168.2.1/24 dev br0 broadcast 192.168.2.255
+    #------------------------------------------------------------
+
+    lan )   qemu-system-x86_64 \
+            -enable-kvm \
+            -smp 2,sockets=2,cores=1,threads=1 \
+            -name puppet01.demo.lan \
+            -nodefconfig \
+            -rtc base=utc \
+            -drive file='/home/marc/CentOS.img',if=virtio \
+            -net nic,model=virtio \
+            -net tap,vlan=0,ifname=tap0,script=no,downscript=no \
+            -serial pty \
+            -m 1024 \
+            -nographic \
+            -daemonize
+            ;;
 esac
