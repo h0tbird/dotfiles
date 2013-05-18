3474e152e4f0f03263f1f2187cdd980b000c1017
Marc Villacorta <marc.villacorta@gmail.com>
2013-05-18 21:37:48 +0200

    Adding KVM counter to statusbar.

--- rootfs/home/marc/bin/statusbar.c
+++ rootfs/home/marc/bin/statusbar.c
@@ -1,5 +1,5 @@
 // Compile with:
-// gcc -Wall -pedantic -std=c99 -lX11 statusbar.c -o statusbar
+// gcc -Wall -pedantic -std=c99 -lX11 -D_BSD_SOURCE statusbar.c -o statusbar
 
 //-----------------------------------------------------------------------------
 // Includes:
@@ -11,11 +11,18 @@
 #include <time.h>
 #include <X11/Xlib.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
 
 //-----------------------------------------------------------------------------
 // Defines:
 //-----------------------------------------------------------------------------
 
+#define CMDLEN 32
 #define MyDBG(x) do {printf("(%d) %s:%d\n", errno, __FILE__, __LINE__); goto x;} while (0)
 
 //-----------------------------------------------------------------------------
@@ -36,34 +43,97 @@ void setstatus(char *str)
 }
 
 //-----------------------------------------------------------------------------
+// getvmcount:
+//-----------------------------------------------------------------------------
+
+int getvmcount()
+
+{
+    DIR *dp;
+    struct dirent *ep;
+    char str[CMDLEN];
+    int fd, c = 0;
+    ssize_t r;
+    unsigned n = 0;
+
+    //------------
+    // Open /proc
+    //------------
+
+    if((dp = opendir("/proc")) == NULL) MyDBG(end0);
+
+    //----------------------------------
+    // Loop through /proc/<pid>/cmdline
+    //----------------------------------
+
+    while((ep = readdir(dp))) { if(ep->d_type == DT_DIR) {
+
+        if(snprintf(str, CMDLEN, "%d", atoi(ep->d_name)) < 0) MyDBG(end0);
+        if(strcmp(str, ep->d_name) != 0) continue;
+        if(snprintf(str, CMDLEN, "/proc/%s/cmdline", ep->d_name) < 0) MyDBG(end0);
+        if((fd = open(str, O_RDONLY)) == -1) MyDBG(end0);
+
+        //---------------
+        // Read cmdline:
+        //---------------
+
+        n=0; str[0] = '\0'; while(1) {
+
+            if((r = read(fd,str+n,CMDLEN-n)) == -1) {
+                if(errno==EINTR) continue;
+                break;
+            }
+
+            n += r;
+            if(n==CMDLEN) break;
+            if(r==0) break;
+        }
+
+        if(n==CMDLEN) n--;
+        str[n] = '\0';
+        close(fd);
+
+        //---------------------------
+        // Count qemu-system-x86_64:
+        //---------------------------
+
+        if(!strcmp(str, "qemu-system-x86_64")) c++;
+    }}
+
+    //--------------------
+    // Return on success:
+    //--------------------
+
+    closedir(dp);
+    return c;
+
+    //------------------
+    // Return on error:
+    //------------------
+
+    end0: return 0;
+}
+
+//-----------------------------------------------------------------------------
 // getdatetime:
 //-----------------------------------------------------------------------------
 
 char *getdatetime()
 
 {
-    char *buf;
-    time_t result;
-    struct tm *resulttm;
+    char *buf = NULL;
+    time_t result = time(NULL);
+    struct tm *resulttm = localtime(&result);
 
-    if((buf = malloc(sizeof(char)*65)) == NULL) {
-        fprintf(stderr, "Cannot allocate memory for buf.\n");
-        exit(1);
-    }
+    if(resulttm == NULL) MyDBG(end0);
+    if((buf = malloc(sizeof(char)*65)) == NULL) MyDBG(end0);
+    if(!strftime(buf, sizeof(char)*65-1, "%a %d %b Ý %H:%M:%S ", resulttm)) MyDBG(end0);
 
-    result = time(NULL);
-    resulttm = localtime(&result);
-    if(resulttm == NULL) {
-        fprintf(stderr, "Error getting localtime.\n");
-        exit(1);
-    }
-
-    if(!strftime(buf, sizeof(char)*65-1, "%a %d %b Ý %H:%M:%S ", resulttm)) {
-        fprintf(stderr, "strftime is 0.\n");
-        exit(1);
-    }
-
+    // Return on success:
     return buf;
+
+    // Return on error:
+    end0: return NULL;
 }
 
 //-----------------------------------------------------------------------------
@@ -76,34 +146,23 @@ int getbattery()
     FILE *fd;
     int energy_now, energy_full, voltage_now;
 
-    fd = fopen("/sys/class/power_supply/BAT0/energy_now", "r");
-    if(fd == NULL) {
-        fprintf(stderr, "Error opening energy_now.\n");
-        return -1;
-    }
-
+    if((fd = fopen("/sys/class/power_supply/BAT0/energy_now", "r")) == NULL) MyDBG(end0);
     fscanf(fd, "%d", &energy_now);
     fclose(fd);
 
-    fd = fopen("/sys/class/power_supply/BAT0/energy_full", "r");
-    if(fd == NULL) {
-        fprintf(stderr, "Error opening energy_full.\n");
-        return -1;
-    }
-
+    if((fd = fopen("/sys/class/power_supply/BAT0/energy_full", "r")) == NULL) MyDBG(end0);
     fscanf(fd, "%d", &energy_full);
     fclose(fd);
 
-    fd = fopen("/sys/class/power_supply/BAT0/voltage_now", "r");
-    if(fd == NULL) {
-        fprintf(stderr, "Error opening voltage_now.\n");
-        return -1;
-    }
-
+    if((fd = fopen("/sys/class/power_supply/BAT0/voltage_now", "r")) == NULL) MyDBG(end0);
     fscanf(fd, "%d", &voltage_now);
     fclose(fd);
 
+    // Return on success:
     return ((float)energy_now * 1000 / (float)voltage_now) * 100 / ((float)energy_full * 1000 / (float)voltage_now);
+
+    // Return on error:
+    end0: return -1;
 }
 
 //-----------------------------------------------------------------------------
@@ -115,16 +174,20 @@ int main(void)
 {
     char *status;
     char *datetime;
-    int bat0;
+    int bat0, vmc;
 
-    if (!(dpy = XOpenDisplay(NULL))) MyDBG(end0);
-    if ((status = malloc(200)) == NULL) MyDBG(end0);
+    if(!(dpy = XOpenDisplay(NULL))) MyDBG(end0);
+    if((status = malloc(200)) == NULL) MyDBG(end0);
 
-    while (1) {
+    while(1) {
 
         datetime = getdatetime();
         bat0 = getbattery();
-        snprintf(status, 200, "\x04Ü\x06 Ã %d%% \x05Ü\x02 Õ %s", bat0, datetime);
+        vmc = getvmcount();
+
+        if(vmc < 1) snprintf(status, 200, "\x04Ü\x06 Ã %d%% \x05Ü\x02 Õ %s", bat0, datetime);
+        else snprintf(status, 200, "\x01Ý \x0CÍ %d \x04Ü\x06 Ã %d%% \x05Ü\x02 Õ %s", vmc, bat0, datetime);
+
         free(datetime);
         setstatus(status);
         sleep(1);
