25d589bc6b4e9627ee39a8dddbbe52214429ec77
Marc Villacorta <marc.villacorta@gmail.com>
2014-05-04 14:25:23 +0200

    Update wireless.sh

--- rootfs/home/marc/bin/wireless.sh
+++ rootfs/home/marc/bin/wireless.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
-sudo iw dev wlan0 scan | grep -i ssid
+echo "#-------------------------------------------------"
+echo "# Networks sorted by signal:"
+echo "#-------------------------------------------------"
+echo
+
+sudo iw dev wlan0 scan | egrep -i "ssid:|signal:" | paste - - | sort
+
+echo
+echo "#-------------------------------------------------"
+echo "# Current selection:"
+echo "#-------------------------------------------------"
+echo
+
 sudo iw dev wlan0 link
+
+echo
